02dff9efe945d9642fa5e9cd9558eb15421057be
Joe Newman <makerslaptop23@Makerss-MacBook-Pro-4.local>
2015-03-04 12:17:18 +0000

    temp control added

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,24 +1,56 @@
-describe("Plane", function() {
-  var plane;
+describe("Thermostat", function() {
+  var thermostat;
 
   beforeEach(function() {
-    plane = new Plane();
+    thermostat = new Thermostat();
   });
 
-  it("should be in the air when created", function() {
-
-    expect(plane.isFlying).toBe(true);
+  it("should have a temperature of 20 degrees when created", function() {
+    expect(thermostat.temperature).toEqual(20);
   });
 
-  it("can land", function(){
-    plane.land();
-    expect(plane.isFlying).toBe(false);
+  it("should be able to increase the temperature with the up button", function() {
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(21);
   });
 
-  it("can take off", function() {
-    plane.land();
-    plane.takeOff();
-    expect(plane.isFlying).toBe(true);
+  it("should be able to increase the temperature with the down button", function() {
+    thermostat.down();
+    expect(thermostat.temperature).toEqual(19);
+  });
+
+  it("should have a minimum temperature of 10 degrees", function() {
+    thermostat.temperature = 10;
+    thermostat.down();
+    expect(thermostat.temperature).toEqual(10);
+  });
+
+  it("should have power saving mode on by default", function() {
+    expect(thermostat.powerSaving).toBe(true);
+  })
+
+  it("should be able to turn power saving off", function() {
+    thermostat.powersavingOff();
+    expect(thermostat.powerSaving).toBe(false);    
+  });
+
+  it("should have a max temp of 25 when power saving is on", function() {
+    thermostat.temperature = 25;
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(25);
+  });
+
+  it("should have a max temp of 32 when power saving is off", function() {
+    thermostat.temperature = 32;
+    thermostat.powersavingOff;
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(32);
+  });
+
+  it("should be able to reset the temperature to 20 by hitting the reset button", function() {
+    thermostat.resetTemp();
+    expect(thermostat.temperature).toEqual(20);
+
   });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,12 +1,29 @@
-var Plane = function() {
-  this.isFlying = true;
+var Thermostat = function() {
+  this.temperature = 20
+  this.powerSaving = true
 };
 
-Plane.prototype.land = function() {
-  this.isFlying = false;
+Thermostat.prototype.up = function() {
+  if (this.powerSaving = true && this.temperature <25) {
+      this.temperature += 1;  
+    }
+  else {
+    if (this.powerSaving = false && this.temperature <32) {
+      this.temperature += 1;  
+    };
+  };
 };
 
-Plane.prototype.takeOff = function(){
-  this.isFlying = true;
+Thermostat.prototype.down = function() {
+  if (this.temperature > 10) {
+    this.temperature -= 1;
+  };
 };
 
+Thermostat.prototype.powersavingOff = function() {
+  this.powerSaving = false;
+};
+
+Thermostat.prototype.resetTemp = function() {
+  this.temperature = 20;
+};
